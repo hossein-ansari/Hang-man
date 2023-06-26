3cb3f24fbcccc350724519c156da78c184761f10
hossein <hossein838485@gmail.com>
2023-06-26 18:16:20 +0430

    add render conditional for hangman component

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import '../style/Hangman.css'
+import React, { useContext } from "react";
+import "../style/Hangman.css";
+import { contextBox } from "../context/Context";
 export default function Hangman() {
+  const data = useContext(contextBox);
   return (
-    <div className='Hangman'>
-      <div className='Foundation'></div>
-      <div className='frame'></div>
-      <div className='pate'></div>
-      <div className='hanging'></div>
-      <div className='head'>
-        <div className='eyes'></div>
-        <div className='eyes'></div>
-        <div className='mouth'></div>
+    <div className="Hangman">
+      <div className="Foundation"></div>
+      <div className="frame"></div>
+      <div className="pate"></div>
+      <div className="hanging"></div>
+      <div className="head">
+        <div className="eyes"></div>
+        <div className="eyes"></div>
+        <div className="mouth"></div>
       </div>
-      <div className='body'></div>
-      <div className='Rhand'></div>
-      <div className='lhand'></div>
-      <div className='Rfoot'></div>
-      <div className='Lfoot'></div>
-
+      <div className="body"></div>
+      <div className="Rhand"></div>
+      <div className="lhand"></div>
+      <div className="Rfoot"></div>
+      <div className="Lfoot"></div>
     </div>
-  )
+  );
 }
