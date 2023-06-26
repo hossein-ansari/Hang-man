150744cdc9b23cf39758c3422231931c0febce86
hossein <hossein838485@gmail.com>
2023-06-26 17:33:48 +0430

    add map on wrongAnswer

--- src/components/Wronganswer.js
+++ src/components/Wronganswer.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../style/Wronganswer.css";
+import { contextBox } from "../context/Context";
 export default function Wronganswer() {
+  const data = useContext(contextBox)
+
   return (
     <div className="Wronganswer">
       <h2 className="headerName">Wrong Answer<hr></hr></h2>
-      
+      <ul className="Wronganswer-Words">
+          {data.wrongAnswersArray.map((element)=>(
+            <li className="wrongWordLi">{element}</li>
+          ))}
+      </ul>
     </div>
   );
 }
