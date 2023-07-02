691f584c6a9f2ff9c2688c23c0fee978b7c65f1e
hossein <hossein838485@gmail.com>
2023-07-02 15:44:50 +0430

    add new image

--- src/components/LostPopUp.js
+++ src/components/LostPopUp.js
@@ -8,21 +8,13 @@ export default function LostPopUp() {
       <div className="LostPop-up">
         <h1 className="lostWord">You Lost</h1>
         <p className="selectedWord">Selected word: {data.mainWord}</p>
-        {/* <button className="reloadBtn"
+        <button className="reloadBtn"
         onClick={() => {
           window.location.reload(false);
         }}>
             Try Again
-      </button> */}
-        <button
-          onClick={() => {
-            window.location.reload(false);
-          }}
-          type="button"
-          class="btn btn-primary reloadBtn"
-        >
-          Try Again
-        </button>
+      </button>
+        
       </div>
     </div>
   );
