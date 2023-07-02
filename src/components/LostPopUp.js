e679f99cb8ef37e3f9cc4eaff2c297f3dd72bdca
hossein <hossein838485@gmail.com>
2023-07-02 15:31:43 +0430

    add image file and image

--- src/components/LostPopUp.js
+++ src/components/LostPopUp.js
@@ -4,24 +4,26 @@ import { contextBox } from "../context/Context";
 export default function LostPopUp() {
   const data = useContext(contextBox);
   return (
-    <div className="LostPop-up">
-      <h1 className="lostWord">You Lost</h1>
-      <p className="selectedWord">Selected word: {data.mainWord}</p>
-      {/* <button className="reloadBtn"
+    <div className="LostPopUpComponent">
+      <div className="LostPop-up">
+        <h1 className="lostWord">You Lost</h1>
+        <p className="selectedWord">Selected word: {data.mainWord}</p>
+        {/* <button className="reloadBtn"
         onClick={() => {
           window.location.reload(false);
         }}>
             Try Again
       </button> */}
-      <button
-        onClick={() => {
-          window.location.reload(false);
-        }}
-        type="button"
-        class="btn btn-primary reloadBtn"
-      >
-        Try Again
-      </button>
+        <button
+          onClick={() => {
+            window.location.reload(false);
+          }}
+          type="button"
+          class="btn btn-primary reloadBtn"
+        >
+          Try Again
+        </button>
+      </div>
     </div>
   );
 }
